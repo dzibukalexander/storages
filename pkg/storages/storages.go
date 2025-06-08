47d778a9003552159e9012948c216e64ee32cac5
Дзибук Александр <as006105@g.bstu.by>
2025-06-08 16:34:59 +0300

    p210

--- pkg/storages/storages.go
+++ pkg/storages/storages.go
@@ -1,7 +1,12 @@
 package storagesssssss
 
-import "github.com/dzibukalexander/storages/v2/internal/storage"
+import (
+	"fmt"
+
+	"github.com/dzibukalexander/storages/v2/internal/storage"
+)
 
 func NewStorage() *storage.Storage {
+	fmt.Println("tag 2.1.0")
 	return storage.NewStorage()
 }
