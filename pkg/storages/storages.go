35f7b38652b1897b27f99753b2cf11f076c057c6
Дзибук Александр <as006105@g.bstu.by>
2025-06-08 16:42:43 +0300

    325 tag

--- pkg/storages/storages.go
+++ pkg/storages/storages.go
@@ -1,7 +1,12 @@
 package storagesssssss
 
-import "github.com/dzibukalexander/storages/v3/internal/storage"
+import (
+	"fmt"
+
+	"github.com/dzibukalexander/storages/v3/internal/storage"
+)
 
 func NewStorage() *storage.Storage {
+	fmt.Println("tag 3.2.5 fixed")
 	return storage.NewStorage()
 }
